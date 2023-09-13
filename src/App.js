c38501088ee8e41608b97196f8bd76fa735e19b4
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-13 17:55:39 +0200

    Add useState hook to show a meme

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
+import { useState } from 'react';
+
 export default function App() {
-  return <></>;
+  const randomMeme = (
+    <img
+      src="https://api.memegen.link/images/aag/_/aliens.png"
+      alt="the meme"
+    />
+  );
+  const [meme, setMeme] = useState(randomMeme.filter);
+
+  // input fields for text and generate button
+
+  return (
+    <>
+      <form>
+        <input placeholder="your top text" />
+        <input placeholder="your bottom text" />
+      </form>
+      <button
+        onClick={() => {
+          const randomCalledMeme = randomMeme;
+          setMeme(randomCalledMeme);
+        }}
+      >
+        we generate your meme
+      </button>
+      {/* Show the meme on the screen */}
+      {meme}
+    </>
+  );
 }
