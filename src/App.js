fc8a33dba952dd5639e27bd69714fec8a380f773
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-17 16:20:29 +0200

    Add onChange event

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
+import axios from 'axios';
 import { useState } from 'react';
-import TestApp from './TestApp';
 
 export default function App() {
-  // const templateURL = 'https://api.memegen.link/images/aag';
-  // const id = props.id;
-  // const randomMeme = templateURL + props.id;
-  // const [meme, setMeme] = useState();
-  // const [memeUrl, setMemeUrl] = useState(
-  //   `https://api.memegen.link/templates/${topText}/${bottomText}.gif?width=300`,
-  // );
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const baseUrl = 'https://api.memegen.link/';
+  const templatesUrl = `${baseUrl}templates/`;
+  // const imageId = `${templatesUrl}`;
+  const imagesUrl = `${baseUrl}images/aag/${topText}/${bottomText}.png`;
+  // const [randomMeme, setRandomMeme] = useState([]);
 
+  axios.get(templatesUrl).then((responseArr) => {
+    console.log(responseArr.data);
+  });
   // input fields for text and generate button
 
   return (
     <>
-      {/* <form>
-        <input placeholder="your top text" />
-        <input placeholder="your bottom text" />
+      <form>
+        <input
+          value={topText}
+          onChange={(event) => setTopText(event.currentTarget.value)}
+          placeholder="your top text"
+        />
+        <input
+          value={bottomText}
+          placeholder="your bottom text"
+          onChange={(event) => setBottomText(event.currentTarget.value)}
+        />
       </form>
-      <button
-        onClick={() => {
-          const randomCalledMeme = randomMeme;
-          setMeme(randomCalledMeme);
-        }}
-      >
+      <button type="button" onClick={(event) => topText}>
         we generate your meme
       </button>
-      {/* Show the meme on the screen */}
-      {/* {meme} */}
-      <TestApp />
+      <div>THIS IS YOUR TOP TEXT: {topText} </div>
+      <div>THIS IS YOUR BOTTOM TEXT: {bottomText} </div>
+      {setTopText}
+      {setBottomText}
     </>
   );
 }
